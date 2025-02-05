5be7c7d2ce05dd1a49a9acf563ad785c15be3eb0
itayf02 <itay.fak@campus.technion.ac.il>
2025-02-05 21:56:16 +0200

    I fixed some of problems Peleg commented on

--- training-main (1)/training-main/intro/python/python/tests.py
+++ training-main (1)/training-main/intro/python/python/tests.py
@@ -1,6 +1,7 @@
-def gen():
-    yield from range(5)
+for num in range(1000, 3001):
+    flag = True
+    num_temp = num
+    while num_temp > 0:
+        if (num_temp % 10) % 2 != 0:
+            flag = False
 
-g = gen()
-print(next(g))
-print(next(g))
